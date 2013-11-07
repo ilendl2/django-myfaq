b13735a685190406fca2fb0a914432a4a447a161
Lendl Smith <lendl.smith@gmail.com>
2013-11-07 14:45:56 -0400

    made to work with Django 1.5

--- faq/urls.py
+++ faq/urls.py
@@ -1,21 +1,14 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
-from faq.views import faq_list_by_group, submit_faq
-
-admin.autodiscover()
+from faq.views import QuestionList
 
 urlpatterns = patterns('',
 
-url (
-    r'^$',
-    faq_list_by_group,
-    name = 'faq',
-    ),
-url (
-    r'^submit_faq/$',
-    submit_faq,
-    {'success_url': '/faq/'},
-    name = 'submit',
-    ),
+    url (
+        regex =r'^$',
+        view = QuestionList.as_view(),
+        name = 'faq',
+        ),
 )
+
 
